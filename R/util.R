9cd88a40f160d24566277174fe5ce07d6e16ed32
dyfan jones <dyfan.jones@shopdirect.com>
2019-08-17 14:56:09 +0100

    added py error handler and updated waiter to check more frequently for query status

--- R/util.R
+++ R/util.R
@@ -21,11 +21,21 @@ client_athena <- function(botosession){
 
 # holds functions until athena query competed
 waiter <- function(res){
-  Athena <- client_athena(res@connection)
   while (TRUE){
-    query_execution <- Athena$get_query_execution(QueryExecutionId = res@info$QueryExecutionId)
+    tryCatch(query_execution <- res@athena$get_query_execution(QueryExecutionId = res@info$QueryExecutionId),
+             error = function(e) py_error(e))
     if (query_execution$QueryExecution$Status$State %in% c("SUCCEEDED", "FAILED", "CANCELLED")){
       return (query_execution)
-    } else {Sys.sleep(2)}
+    } else {Sys.sleep(1)}
   }
 }
+
+# python error handler
+py_error <- function(e){
+  e$call <- sys.calls()[[1]]
+  if (sys.nframe() > 5) {e$call <- sys.calls()[[sys.nframe() - 5]]}
+  pe <- py_last_error()
+  if (!is.null(pe)) {e$message <- paste0('Python `', pe$type, '`: ', pe$value)}
+  stop(e)}
+
+
