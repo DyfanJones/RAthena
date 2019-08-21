7223f012d8f097da44ae21ec7ad5dad8f2015c7a
dyfan jones <dyfan.jones@shopdirect.com>
2019-08-21 18:31:43 +0100

    Updated data type between R and athena

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -3,26 +3,14 @@ AthenaDataType <-
     switch(
       class(fields)[1],
       logical =   "BOOLEAN",
-      integer =   "INTEGER",
+      integer =   "INT",
       integer64 = "BIGINT",
       numeric =   "FLOAT",
-      factor =    char_type(fields),
-      character = char_type(fields),
-      list = list_type(fields),
+      factor =    "STRING",
+      character = "STRING",
+      list = "STRING",
       Date =      "DATE",
       POSIXct =   "TIMESTAMP",
       stop("Unknown class ", paste(class(fields), collapse = "/"), call. = FALSE)
     )
   }
-
-char_type <- function(x) {
-  n <- max(nchar(as.character(x), "bytes"), 0L, na.rm = TRUE)
-  if (n <= 65535) {
-    paste0("varchar(", n, ")")
-  } else {stop("Unable to determine character size")}
-}
-
-list_type <- function(x) {
-  max_length <- max(nchar(as.character(x)), na.rm = TRUE)
-  paste0("VARCHAR (", max_length, ")")
-}
