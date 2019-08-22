09489af7f04bbff50e290568e1f2765ce158625c
dyfan jones <dyfan.jones@shopdirect.com>
2019-08-22 21:19:05 +0100

    Updated data transfer test the returned data only from the partition data of the test

--- tests/testthat/test-datatransfer.R
+++ tests/testthat/test-datatransfer.R
@@ -13,6 +13,6 @@ test_that("Testing data transfer between R and athena", {
   dbWriteTable(con, "test_df", df, overwrite = T, partition = c("timesTamp" = format(Sys.Date(), "%Y%m%d")), s3.location = s3.location)
   
   # if data.table is available in namespace result returned as data.table
-  test_df <- as.data.frame(dbReadTable(con, "test_df"))
-  expect_equal(test_df[,1:2],df)
+  test_df <- as.data.frame(dbGetQuery(con, paste0("select x, y from test_df where timestamp ='",format(Sys.Date(), "%Y%m%d"),"'")))
+  expect_equal(test_df,df)
 })
