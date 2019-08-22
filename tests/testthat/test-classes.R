25abff57f4373cb0db884027e32ecb13e2c6a594
dyfan jones <dyfan.jones@shopdirect.com>
2019-08-22 19:41:26 +0100

    test if s4 class are correctly built

---
+++ tests/testthat/test-classes.R
@@ -0,0 +1,18 @@
+context("classes")
+
+test_that("Testing class formation", {
+  skip_if_no_boto()
+  con <- DBI::dbConnect(RAthena::athena(),
+                   profile_name = "rathena",
+                   s3_staging_dir = "s3://test-rathena/athena-query-results/")
+
+  res <- DBI::dbSendQuery(con, "show databases")
+  DBI::dbClearResult(res)
+
+
+  # testing components of s4 class
+  expect_identical(names(attributes(con)), c("ptr", "info", "quote","class"))
+  expect_identical(names(attributes(res)), c("connection", "athena", "info", "class"))
+  expect_s4_class(con,"AthenaConnection")
+  expect_s4_class(res,"AthenaQuery")
+})
