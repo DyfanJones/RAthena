8f94468830612e4daa82f252e55826fe333705d6
dyfan jones <dyfan.jones@shopdirect.com>
2019-09-02 13:57:15 +0100

    Added logic variable type to data.frame for data to be transferred over to athena

--- tests/testthat/test-datatransfer.R
+++ tests/testthat/test-datatransfer.R
@@ -9,11 +9,14 @@ test_that("Testing data transfer between R and athena", {
                    profile_name = "rathena",
                    s3_staging_dir = "s3://test-rathena/athena-query-results/")
   
-  df <- data.frame(x = 1:10, y = letters[1:10], stringsAsFactors = F)
+  df <- data.frame(x = 1:10,
+                   y = letters[1:10], 
+                   z = sample(c(TRUE, FALSE), 10, replace = T),
+                   stringsAsFactors = F)
   
   dbWriteTable(con, "test_df", df, overwrite = T, partition = c("timesTamp" = format(Sys.Date(), "%Y%m%d")), s3.location = s3.location)
   
   # if data.table is available in namespace result returned as data.table
-  test_df <- as.data.frame(dbGetQuery(con, paste0("select x, y from test_df where timestamp ='",format(Sys.Date(), "%Y%m%d"),"'")))
+  test_df <- as.data.frame(dbGetQuery(con, paste0("select x, y, z from test_df where timestamp ='",format(Sys.Date(), "%Y%m%d"),"'")))
   expect_equal(test_df,df)
 })
