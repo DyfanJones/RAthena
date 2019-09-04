ef4c5eb143336e349acd7df83dfda9dcd93c2717
DyfanJones <dyfan.jones@shopdirect.com>
2019-09-04 16:39:03 +0100

    Created documentation for s3 method for dplyr db_desc

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -1,4 +1,12 @@
 #' S3 implementation of \code{db_desc} for Athena
+#' 
+#' This is a backend function for dplyr to retrieve meta data about Athena queries. Users won't be required to access and run this function.
+#' @param x A \code{\link{dbConnect}} object, as returned by \code{dbConnect()}
+#' 
+#' @return
+#' Character variable containing Meta Data about query sent to Athena. The Meta Data is returned in the following format:
+#' 
+#' \code{"Athena <boto3 version> [<profile_name>@region/database]"}
 db_desc.AthenaConnection <- function(x) {
   info <- dbGetInfo(x)
   profile <- if(!is.null(info$profile_name)) paste0(info$profile_name, "@")
