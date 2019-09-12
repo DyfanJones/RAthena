58b0a35472392a84697e03117a380cc464208944
DyfanJones <dyfan.jones@shopdirect.com>
2019-09-12 14:38:37 +0100

    set up test helper functions to use system variables

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -15,7 +15,7 @@ ROW FORMAT DELIMITED
 	FIELDS TERMINATED BY ','
 	ESCAPED BY '\\\\'
 	LINES TERMINATED BY ", gsub("_","","'\\_n'"),
-"\nLOCATION 's3://test-rathena/removeable_table/test_df/'
+"\nLOCATION '",Sys.getenv("rathena_test_df"),"'
 TBLPROPERTIES (\"skip.header.line.count\"=\"1\");")),
 tbl2 = 
 DBI::SQL(paste0("CREATE EXTERNAL TABLE test_df (
@@ -26,42 +26,42 @@ ROW FORMAT DELIMITED
 	FIELDS TERMINATED BY '\t'
 	ESCAPED BY '\\\\'
 	LINES TERMINATED BY ", gsub("_","","'\\_n'"),
-           "\nLOCATION 's3://test-rathena/removeable_table/test_df/'
+           "\nLOCATION '",Sys.getenv("rathena_test_df"),"'
 TBLPROPERTIES (\"skip.header.line.count\"=\"1\");")), 
 tbl3 = 
-  DBI::SQL("CREATE EXTERNAL TABLE test_df (
+  DBI::SQL(paste0("CREATE EXTERNAL TABLE test_df (
   x INT,
   y STRING
 )
 STORED AS PARQUET
-LOCATION 's3://test-rathena/removeable_table/test_df/'\n;"),
+LOCATION '",Sys.getenv("rathena_test_df"),"'\n;")),
 tbl4 = 
-  DBI::SQL("CREATE EXTERNAL TABLE test_df (\n  x INT,\n  y STRING\n)
+  DBI::SQL(paste0("CREATE EXTERNAL TABLE test_df (\n  x INT,\n  y STRING\n)
 PARTITIONED BY (timestamp STRING)
 STORED AS PARQUET
-LOCATION 's3://test-rathena/removeable_table/test_df/'\n;"))
+LOCATION '",Sys.getenv("rathena_test_df"),"'\n;")))
 
 
 # static Athena Query Request Tests
 athena_test_req1 <-
   list(QueryString = "select * from test_query",
        QueryExecutionContext = list(Database = "default"),
-       ResultConfiguration = list(OutputLocation = "s3://test-rathena/athena-query/",
+       ResultConfiguration = list(OutputLocation = Sys.getenv("rathena_s3"),
                                   EncryptionConfiguration = list(EncryptionOption = "SSE_S3",
                                                                  KmsKey = "test_key")),
        WorkGroup = "test_group")
 athena_test_req2 <-
   list(QueryString = "select * from test_query",
        QueryExecutionContext = list(Database = "default"),
-       ResultConfiguration = list(OutputLocation = "s3://test-rathena/athena-query/",
+       ResultConfiguration = list(OutputLocation = Sys.getenv("rathena_s3"),
                                   EncryptionConfiguration = list(EncryptionOption = "SSE_S3")),
        WorkGroup = "test_group")
 athena_test_req3 <-
   list(QueryString = "select * from test_query",
        QueryExecutionContext = list(Database = "default"),
-       ResultConfiguration = list(OutputLocation = "s3://test-rathena/athena-query/"),
+       ResultConfiguration = list(OutputLocation = Sys.getenv("rathena_s3")),
        WorkGroup = "test_group")
 athena_test_req4 <-
   list(QueryString = "select * from test_query",
        QueryExecutionContext = list(Database = "default"),
-       ResultConfiguration = list(OutputLocation = "s3://test-rathena/athena-query/"))
+       ResultConfiguration = list(OutputLocation = Sys.getenv("rathena_s3")))
