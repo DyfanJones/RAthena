8c3b84e460771734664f703565e4b74298a8eb71
DyfanJones <dyfan.jones@shopdirect.com>
2019-09-18 16:50:42 +0100

    added extra test for increased number of partitioning

--- tests/testthat/test-datatransfer.R
+++ tests/testthat/test-datatransfer.R
@@ -5,7 +5,8 @@ context("data transfer")
 # Sys.getenv("rathena_s3_query"): "s3://path/to/query/bucket/"
 # Sys.getenv("rathena_s3_tbl"): "s3://path/to/bucket/"
 
-s3.location <- paste0(Sys.getenv("rathena_s3_tbl"),"test_df/")
+s3.location1 <- paste0(Sys.getenv("rathena_s3_tbl"),"test_df/")
+s3.location2 <- Sys.getenv("rathena_s3_tbl")
 
 test_that("Testing data transfer between R and athena", {
   skip_if_no_boto()
@@ -20,9 +21,18 @@ test_that("Testing data transfer between R and athena", {
                    z = sample(c(TRUE, FALSE), 10, replace = T),
                    stringsAsFactors = F)
   
-  dbWriteTable(con, "test_df", df, overwrite = T, partition = c("timesTamp" = format(Sys.Date(), "%Y%m%d")), s3.location = s3.location)
+  DATE <- Sys.Date()
+  dbWriteTable(con, "test_df", df, overwrite = T, partition = c("timesTamp" = format(DATE, "%Y%m%d")), s3.location = s3.location1)
+  dbWriteTable(con, "test_df2", df, 
+               overwrite = T,
+               partition = c("year" = format(DATE, "%Y"),
+                             "month" = format(DATE, "%m"),
+                             "DAY" = format(DATE, "%d")),
+               s3.location = s3.location2)
   
   # if data.table is available in namespace result returned as data.table
   test_df <- as.data.frame(dbGetQuery(con, paste0("select x, y, z from test_df where timestamp ='",format(Sys.Date(), "%Y%m%d"),"'")))
+  test_df2 <- as.data.frame(dbGetQuery(con, paste0("select x, y, z from test_df2 where year = '",format(DATE, "%Y"), "' and month = '",format(DATE, "%m"), "' and day = '", format(DATE, "%d"),"'")))
   expect_equal(test_df,df)
+  expect_equal(test_df2,df)
 })
