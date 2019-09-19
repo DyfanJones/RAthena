17a69881038de259ec4fe3a542c62c8fb1f35c1f
DyfanJones <dyfan.jones@shopdirect.com>
2019-09-19 19:16:29 +0100

    removed old code

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -10,7 +10,6 @@
 db_desc.AthenaConnection <- function(x) {
   info <- dbGetInfo(x)
   profile <- if(!is.null(info$profile_name)) paste0(info$profile_name, "@")
-  region_name <- paste0("Region:",info$region_name)
   paste0("Athena ",info$boto3," [",profile,info$region_name,"/", info$dbms.name,"]")
 }
 
