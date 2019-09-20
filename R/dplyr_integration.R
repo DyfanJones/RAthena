443d3f18a17269958844a0908fa97fcb2277c17b
DyfanJones <dyfan.jones@shopdirect.com>
2019-09-20 11:30:18 +0100

    added examples for dplyr methods

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -20,11 +20,44 @@ db_desc.AthenaConnection <- function(x) {
 #' @param con A \code{\link{dbConnect}} object, as returned by \code{dbConnect()}
 #' @param table Table name if left default RAthena will use default from 'dplyr''s \code{compute} function.
 #' @param sql SQL code to be sent to the data
-#' @param ... passes \code{RAthena} table creation parameters: [\code{file.type},\code{s3.location},\code{partition}]
+#' @param ... passes \code{RAthena} table creation parameters: [\code{file_type},\code{s3_location},\code{partition}]
 #' @name db_compute
 #' @return
 #' db_compute returns table name
 #' @seealso \code{\link{db_save_query}}
+#' \dontrun{
+#' # Note: 
+#' # - Require AWS Account to run below example.
+#' # - Different connection methods can be used please see `RAthena::dbConnect` documnentation
+#' 
+#' library(DBI)
+#' library(dplyr)
+#' 
+#' # Demo connection to Athena using profile name 
+#' con <- dbConnect(RAthena::athena(),
+#'                  profile_name = "YOUR_PROFILE_NAME",
+#'                  s3_staging_dir = "s3://path/to/query/bucket/")
+#' 
+#' # Write data.frame to Athena table
+#' copy_to(con, mtcars,
+#'         s3_location = "s3://mybucket/data/")
+#'              
+#' # Write Athena table from tbl_sql
+#' athena_mtcars <- tbl(con, "mtcars")
+#' mtcars_filter <- athena_mtcars %>% filter(gear >=4)
+#' 
+#' # create athena with unique table name
+#' mtcars_filer %>% 
+#'   compute()
+#' 
+#' # create athena with specified name and s3 location
+#' mtcars_filer %>% 
+#'     compute("mtcars_filer",
+#'             s3_location = "s3://mybucket/mtcars_filer/"))
+#' 
+#' # Disconnect from Athena
+#' dbDisconnect(con)
+#' }
 db_compute.AthenaConnection <- function(con,
                                         table,
                                         sql,
@@ -42,22 +75,22 @@ db_compute.AthenaConnection <- function(con,
 #' @param con A \code{\link{dbConnect}} object, as returned by \code{dbConnect()}
 #' @param sql SQL code to be sent to the data
 #' @param name Table name if left default RAthena will use default from 'dplyr''s \code{compute} function.
-#' @param file.type What file type to store data.frame on s3, RAthena currently supports ["NULL","csv", "parquet", "json"]. 
-#'                  \code{"NULL"} will let athena set the file.type for you.
-#' @param s3.location s3 bucket to store Athena table, must be set as a s3 uri for example ("s3://mybucket/data/")
+#' @param file_type What file type to store data.frame on s3, RAthena currently supports ["NULL","csv", "parquet", "json"]. 
+#'                  \code{"NULL"} will let athena set the file_type for you.
+#' @param s3_location s3 bucket to store Athena table, must be set as a s3 uri for example ("s3://mybucket/data/")
 #' @param partition Partition Athena table, requires to be a partitioned variable from previous table.
 #' @param ... other parameters, currently not implemented
 #' @name db_save_query
 #' @return
 #' db_save_query returns table name
 db_save_query.AthenaConnection <- function(con, sql, name , 
-                                           file.type = c("NULL","csv", "parquet", "json"),
-                                           s3.location = NULL,
+                                           file_type = c("NULL","csv", "parquet", "json"),
+                                           s3_location = NULL,
                                            partition = NULL,
                                            ...){
-  stopifnot(is.null(s3.location) || is.s3_uri(s3.location))
-  file.type = match.arg(file.type)
-  tt_sql <- paste0("CREATE TABLE ",name, " ", db_save_query_with(file.type, s3.location, partition), "AS ",
+  stopifnot(is.null(s3_location) || is.s3_uri(s3_location))
+  file_type = match.arg(file_type)
+  tt_sql <- paste0("CREATE TABLE ",name, " ", db_save_query_with(file_type, s3_location, partition), "AS ",
                    sql, ";")
   res <- dbExecute(con, tt_sql)
   # check if execution failed
@@ -69,20 +102,20 @@ db_save_query.AthenaConnection <- function(con, sql, name ,
 }
 
 # helper function
-db_save_query_with <- function(file.type, s3.location,partition){
-  if(file.type!="NULL" || !is.null(s3.location) || !is.null(partition)){
-    FILE <- switch(file.type,
+db_save_query_with <- function(file_type, s3_location,partition){
+  if(file_type!="NULL" || !is.null(s3_location) || !is.null(partition)){
+    FILE <- switch(file_type,
                    "csv" = "format = 'TEXTFILE'",
                    "parquet" = "format = 'PARQUET'",
                    "json" = "format = 'JSON'",
                    "")
-    LOCATION <- if(!is.null(s3.location)){
-      if(file.type == "NULL") paste0("external_location ='", s3.location, "'")
-      else paste0(",\nexternal_location ='", s3.location, "'")
+    LOCATION <- if(!is.null(s3_location)){
+      if(file_type == "NULL") paste0("external_location ='", s3_location, "'")
+      else paste0(",\nexternal_location ='", s3_location, "'")
       } else ""
     PARTITION <- if(!is.null(partition)){
       partition <- paste(partition, collapse = "','")
-      if(is.null(s3.location) && file.type == "NULL") paste0("partitioned_by = ARRAY['",partition,"']")
+      if(is.null(s3_location) && file_type == "NULL") paste0("partitioned_by = ARRAY['",partition,"']")
       else paste0(",\npartitioned_by = ARRAY['",partition,"']")
     } else ""
     paste0("WITH (", FILE, LOCATION, PARTITION,")\n")
@@ -111,6 +144,42 @@ db_save_query_with <- function(file.type, s3.location,partition){
 #' @seealso \code{\link{AthenaWriteTables}}
 #' @return
 #' db_copy_to returns table name
+#' @examples 
+#' \dontrun{
+#' # Note: 
+#' # - Require AWS Account to run below example.
+#' # - Different connection methods can be used please see `RAthena::dbConnect` documnentation
+#' 
+#' library(DBI)
+#' library(dplyr)
+#' 
+#' # Demo connection to Athena using profile name 
+#' con <- dbConnect(RAthena::athena(),
+#'                  profile_name = "YOUR_PROFILE_NAME",
+#'                  s3_staging_dir = "s3://path/to/query/bucket/")
+#' 
+#' # List existing tables in Athena
+#' dbListTables(con)
+#' 
+#' # Write data.frame to Athena table
+#' copy_to(con, mtcars,
+#'         s3_location = "s3://mybucket/data/")
+#'              
+#' # Checking if uploaded table exists in Athena
+#' dbExistsTable(con, "mtcars")
+#'
+#' # Write Athena table from tbl_sql
+#' athena_mtcars <- tbl(con, "mtcars")
+#' mtcars_filter <- athena_mtcars %>% filter(gear >=4)
+#' 
+#' copy_to(con, mtcars_filter)
+#' 
+#' # Checking if uploaded table exists in Athena
+#' dbExistsTable(con, "mtcars_filter") 
+#' 
+#' # Disconnect from Athena
+#' dbDisconnect(con)
+#' }
 
 db_copy_to.AthenaConnection <- function(con, table, values,
                                         overwrite = FALSE, append = FALSE,
