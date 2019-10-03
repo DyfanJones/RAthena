acd288b66b7dbda129c6863befefb46a46dcf525
DyfanJones <dyfan.jones@shopdirect.com>
2019-10-03 10:26:29 +0100

    added a time format to return time in mm:ss even when less then 10

--- R/util.R
+++ R/util.R
@@ -168,8 +168,12 @@ get_aws_env <- function(x) {
 # time check warning when connection will expire soon
 time_check <- function(x){ 
   x <- as.numeric(x - Sys.time(), units = "secs") 
-  if(x %/% 60 < 15) warning("Athena Connection will expire in " ,x %/% 60, ":",round(x %% 60, 0), " (mm:ss)",
+  m <- x %/% 60
+  s <- round(x %% 60, 0)
+  if(m < 15) warning("Athena Connection will expire in ",time_format(m), ":",time_format(s)," (mm:ss)",
                             call. = F)}
+
+time_format <- function(x) if(x < 10) paste0(0,x) else x
 
 # get parent pkg function and method
 pkg_method <- function(fun, pkg) {
