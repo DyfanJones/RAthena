5add5920ab2cfbbfafe729ec681df1764637b165
Ossi Lehtinen <ossi.lehtinen@sok.fi>
2019-11-29 07:58:39 +0000

    Include Presto specific SQL translations from the RPresto package at https://github.com/prestodb/RPresto

---
+++ R/sql_translate_env.R
@@ -0,0 +1,87 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree at https://github.com/prestodb/RPresto.
+
+#' @include dbplyr_compatible.R Connection.R
+NULL
+
+athena_window_functions <- function() {
+  base_win <- dbplyr_compatible('base_win')
+  if (utils::packageVersion('dplyr') < '0.5.0.9004') {
+    return(base_win)
+  }
+  sql_translator <- dbplyr_compatible('sql_translator')
+  win_absent <- dbplyr_compatible('win_absent')
+  win_recycled <- dbplyr_compatible('win_recycled')
+  return(sql_translator(
+    .parent=base_win,
+    all=win_recycled('bool_and'),
+    any=win_recycled('bool_or'),
+    n_distinct=win_absent('n_distinct'),
+    sd=win_recycled("stddev_samp")
+  ))
+}
+
+#' S3 implementation of \code{sql_translate_env} for Athena
+#'
+#' @rdname dplyr_function_implementations
+#' @keywords internal
+#'
+#' @rawNamespace
+#' if (getRversion() >= "3.6.0") {
+#'   S3method(dplyr::sql_translate_env,AthenaConnection)
+#' } else {
+#'   export(sql_translate_env.PrestoConnection)
+#' }
+sql_translate_env.AthenaConnection <- function(con) {
+  sql_variant <- dbplyr_compatible('sql_variant')
+  sql_translator <- dbplyr_compatible('sql_translator')
+  sql_prefix <- dbplyr_compatible('sql_prefix')
+  sql_cast <- dbplyr_compatible('sql_cast')
+  sql <- dbplyr_compatible('sql')
+  build_sql <- dbplyr_compatible('build_sql')
+  base_scalar <- dbplyr_compatible('base_scalar')
+  base_agg <- dbplyr_compatible('base_agg')
+  return(sql_variant(
+    sql_translator(.parent = base_scalar,
+      ifelse = sql_prefix("IF"),
+      as = function(column, type) {
+        sql_type <- stringi::stri_trans_toupper(
+          dbDataType(athena(), type),
+          'en_US.UTF-8'
+        )
+        build_sql('CAST(', column, ' AS ', sql(sql_type), ')')
+      },
+      as.character = sql_cast("VARCHAR"),
+      as.numeric = sql_cast("DOUBLE"),
+      as.double = sql_cast("DOUBLE"),
+      as.integer = sql_cast("BIGINT"),
+      as.Date = sql_cast("DATE"),
+      as.logical = sql_cast("BOOLEAN"),
+      as.raw = sql_cast("VARBINARY"),
+      tolower = sql_prefix("LOWER"),
+      toupper = sql_prefix("UPPER"),
+      pmax = sql_prefix("GREATEST"),
+      pmin = sql_prefix("LEAST"),
+      is.finite = sql_prefix("IS_FINITE"),
+      is.infinite = sql_prefix("IS_FINITE"),
+      is.nan = sql_prefix("IS_NAN"),
+      `[[` = function(x, i) {
+        if (is.numeric(i) && isTRUE(all.equal(i, as.integer(i)))) {
+          i <- as.integer(i)
+        }
+        dbplyr::build_sql(x, "[", i, "]")
+      }
+    ),
+    sql_translator(.parent = base_agg,
+      n = function() sql("COUNT(*)"),
+      sd =  sql_prefix("STDDEV_SAMP"),
+      var = sql_prefix("VAR_SAMP"),
+      all = sql_prefix("BOOL_AND"),
+      any = sql_prefix("BOOL_OR")
+    ),
+    athena_window_functions()
+  ))
+}
