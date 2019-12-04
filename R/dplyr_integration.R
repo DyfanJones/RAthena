334baa7ff2feeef0b5b26cca2198a5249d08cefa
DyfanJones <dyfan.jones@shopdirect.com>
2019-12-04 10:41:28 +0000

    added quote_identifier to compute method

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -94,7 +94,8 @@ db_save_query.AthenaConnection <- function(con, sql, name ,
                                            ...){
   stopifnot(is.null(s3_location) || is.s3_uri(s3_location))
   file_type = match.arg(file_type)
-  tt_sql <- paste0("CREATE TABLE ",name, " ", db_save_query_with(file_type, s3_location, partition), "AS ",
+  tt_sql <- paste0("CREATE TABLE ",paste0(quote_identifier(con,  unlist(strsplit(table,"\\."))), collapse = "."),
+                   " ", db_save_query_with(file_type, s3_location, partition), "AS ",
                    sql, ";")
   res <- dbExecute(con, tt_sql)
   # check if execution failed
