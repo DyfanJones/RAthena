d648d709893d097dc60de0c82a193514f3f28521
DyfanJones <dyfan.jones@shopdirect.com>
2019-12-06 11:27:10 +0000

    fixed bug: path not created correctly, data.frame splitter

--- R/util.R
+++ R/util.R
@@ -188,11 +188,11 @@ pkg_method <- function(fun, pkg) {
 # split data frame into batches
 split_data <- function(x, max.batch = Inf, path = tempdir(), sep = ",", compress = T, file.type = "csv"){
   
-  
   # Bypass splitter if not compressed
   if(!compress){
     file <- paste(paste(sample(letters, 10, replace = TRUE), collapse = ""), Compress(file.type, compress), sep = ".")
-    fwrite(x, file, sep = sep, showProgress = FALSE)
+    path <- file.path(path, file)
+    fwrite(x, path, sep = sep, showProgress = FALSE)
     return(path)}
   
   # set up split vec
@@ -215,7 +215,7 @@ split_data <- function(x, max.batch = Inf, path = tempdir(), sep = ",", compress
 }
 
 write_batch <- function(split_vec, dt, max.batch, max_row, path, sep, compress, file.type){
-  sample <- dt[split_vec:min(max_row,(split_vec+max.batch-1))]
+  sample <- dt[split_vec:min(max_row,(split_vec+max.batch-1)),]
   file <- paste(paste(sample(letters, 10, replace = TRUE), collapse = ""), Compress(file.type, compress), sep = ".")
   path <- file.path(path, file)
   fwrite(sample, path, sep = sep, showProgress = FALSE)
