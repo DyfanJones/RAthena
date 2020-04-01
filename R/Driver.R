49b5ab778647dbd1461d0856354d532839cac01b
DyfanJones <dyfan.jones@shopdirect.com>
2020-04-01 09:08:12 +0100

    fixed example bug

--- R/Driver.R
+++ R/Driver.R
@@ -101,7 +101,7 @@ setMethod(
 #' \dontrun{
 #' # Connect to Athena using your aws access keys
 #'  library(DBI)
-#'  con <- dbConnect(RAthena:athena(),
+#'  con <- dbConnect(RAthena::athena(),
 #'                   aws_access_key_id='YOUR_ACCESS_KEY_ID', # 
 #'                   aws_secret_access_key='YOUR_SECRET_ACCESS_KEY',
 #'                   s3_staging_dir='s3://path/to/query/bucket/',
