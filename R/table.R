84f6235300a5187de49cbdf9a298298c6d49623b
DyfanJones <dyfan.jones@shopdirect.com>
2020-04-30 10:46:15 +0100

    fixed example

--- R/table.R
+++ R/table.R
@@ -393,12 +393,12 @@ setMethod("sqlData", "AthenaConnection",
 #' 
 #' # Create DDL for iris data.frame with partition
 #' sqlCreateTable(con, "iris", iris, 
-#'                partition = c("timestamp" = format(Sys.Date(), "%Y%m%d")),
+#'                partition = "timestamp",
 #'                s3.location = "s3://path/to/athena/table")
 #'                
 #' # Create DDL for iris data.frame with partition and file.type parquet
 #' sqlCreateTable(con, "iris", iris, 
-#'                partition = c("timestamp" = format(Sys.Date(), "%Y%m%d")),
+#'                partition = "timestamp",
 #'                s3.location = "s3://path/to/athena/table",
 #'                file.type = "parquet")
 #' 
