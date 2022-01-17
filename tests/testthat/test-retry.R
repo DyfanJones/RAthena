28f263416a150e70030ddbe2efcb403aa1bc5edb
Dyfan Jones <dyfan.jones@theverygroup.com>
2022-01-17 09:28:56 +0000

    feature: check for deperecated warning message

--- tests/testthat/test-retry.R
+++ tests/testthat/test-retry.R
@@ -29,5 +29,6 @@ test_that("Check if retry_api is working as intended",{
   expect_error(retry_api_call(fail_function(fail_env$i, 3)))
   
   expect_error(RAthena_options(retry = - 10))
-  expect_error(RAthena_options(retry_quiet = "blah"))
+  expect_warning(RAthena_options(retry_quiet = "blah"))
+  expect_error(RAthena_options(verbose = "blah"))
 })
